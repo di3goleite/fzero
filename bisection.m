e3171baeb28b5d61aff65718b292ef16ecdb461d
Diego Leite <diegojleite@gmail.com>
2016-08-31 00:08:51 -0300

    Debug mode

--- bisection.m
+++ bisection.m
@@ -4,37 +4,42 @@ function bisection(f, a, b, err, max_its)
     fa = f(a);
     fb = f(b);
     xr = 0;
-    last_xr = 0;
 
     while(it <= max_its)
         it = it + 1;
         printf("It: %d\n", it);
+        last_xr = xr;
         xr = (a + b) / 2;
         fxr = f(xr);
+
+        printf("-------------------\n");
+        printf("fa: %d\n", fa);
+        printf("fxr: %d\n", fxr);
 
         if(fxr == 0)
             break;
         elseif(fxr * fa < 0)
+            printf("negativo\n");
             b = xr;
         else
+            printf("positivo\n");
             a = xr;
             fa = fxr;
         endif
 
-        if(it > 1)
-            e = abs((xr-last_xr)/xr);
-            last_xr = xr;
+        e = abs((xr-last_xr)/xr);
 
-            printf("E: %d\n", e);
-            printf("Err: %d\n", err);
-            if(e <= err)
-               break;
-            endif
-        else
-            last_xr = xr;
-        endif
+        printf("xr: %d\n", xr);
+        printf("lxr: %d\n", last_xr);
+        printf("e: %d\n", e);
+        printf("err: %d\n", err);
         printf("\n");
+
+        if(it == 19)
+            break;
+        endif
     endwhile
 
+    printf("====================\n");
     printf("xr: %d\n", xr);
 endfunction
